af8cb6ffd277ff72ae60f5bc48cb89acdfe89b7b
zhukboh <bzhuk@cogniance.com>
2015-09-22 23:05:16 +0300

    percolation finished

---
+++ assignment1/src/main/java/PercolationStats.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private final int gridSize;
+    private final int numberOfExperiments;
+    private final double[] experimentResults;
+
+    public PercolationStats(int N, int T) {
+        gridSize = N;
+        numberOfExperiments = T;
+        experimentResults = new double[numberOfExperiments];
+
+        for (int loop = 0; loop < numberOfExperiments; loop++) {
+            Percolation percolation = new Percolation(gridSize);
+            int experimentResult = 0;
+            while (!percolation.percolates()) {
+                int i = StdRandom.uniform(1, gridSize + 1);
+                int j = StdRandom.uniform(1, gridSize + 1);
+                if (!percolation.isOpen(i, j)) {
+                    percolation.open(i, j);
+                    experimentResult++;
+                }
+            }
+            double fraction = experimentResult / (gridSize * gridSize);
+            experimentResults[loop] = fraction;
+        }
+
+    }
+
+    public double mean() {
+        return StdStats.mean(experimentResults);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(experimentResults);
+    }
+
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(numberOfExperiments);
+    }
+
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(numberOfExperiments);
+    }
+}
