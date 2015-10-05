9aea753a63527b5fa8a8ad1b3f208767fa4f9010
Bohdan Zhuk <bzhuk@cogniance.com>
2015-10-05 19:00:08 +0300

    Base Point without implementation

--- assignment3/src/main/java/Point.java
+++ assignment3/src/main/java/Point.java
@@ -1,13 +1,31 @@
+import edu.princeton.cs.algs4.StdDraw;
+
 import java.util.Comparator;
 
-public final class Point implements Comparable<Point>{
-    public Point(int x, int y)                         // constructs the point (x, y)
+public final class Point implements Comparable<Point> {
+    private final int x;
+    private final int y;
 
-    public   void draw()                               // draws this point
-    public   void drawTo(Point that)                   // draws the line segment from this point to that point
-    public String toString()                           // string representation
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-    public               int compareTo(Point that)     // compare two points by y-coordinates, breaking ties by x-coordinates
-    public            double slopeTo(Point that)       // the slope between this point and that point
-    public Comparator<Point> slopeOrder()              // compare two points by slopes they make with this point
-}
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public int compareTo(Point that) {return 0;}
+    public double slopeTo(Point that) {
+        return (double) (this.y - that.y) / ((double) (this.x - that.x));
+    }
+
+    public Comparator<Point> slopeOrder() {return null;}}
